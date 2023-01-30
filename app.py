c10530c6cf931025b9fe919a2b07bf327332aa98
Bruno Brasil <70863338+brunobf09@users.noreply.github.com>
2023-01-29 23:54:25 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+#Import
+import streamlit as st
+# import func as f
+
+st.set_page_config(page_title="Esquadrão Arara")
+
+#Page desing
+st.title('**Esquadrão Arara**')
+st.text('por: Bruno Brasil')
+st.image("img.png")
